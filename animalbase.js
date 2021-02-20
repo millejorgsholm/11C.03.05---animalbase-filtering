9e5304818549417503241d2b0cfa574d3eb16a54
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-20 12:15:04 +0100

    filtering is working

--- animalbase.js
+++ animalbase.js
@@ -3,8 +3,9 @@
 window.addEventListener("DOMContentLoaded", start);
 
 let allAnimals = [];
+let filter;
 
-// The prototype for all animals:
+//Creating the prototype for all animals:
 const Animal = {
   name: "",
   desc: "-unknown animal-",
@@ -12,71 +13,43 @@ const Animal = {
   age: 0,
 };
 
-function start() {
-  console.log("ready");
+const allFilterButtons = document.querySelectorAll(
+  `button.filter[data-action="filter"`
+);
 
-  // TODO: Add event-listeners to filter and sort buttons
-  addEventListenersToButtons();
+function start() {
+  console.log("start");
 
   loadJSON();
+
+  // TODO: Add event-listeners to sort buttons
+
+  // Add event-listeners to filter-buttons
+  allFilterButtons.forEach(filterButton => {
+    filterButton.addEventListener("click", clickFilterButton);
+  });
 }
 
-//Register buttons
-function addEventListenersToButtons() {
-  console.log("addEventListenersToButtons");
-  document
-    .querySelector("[data-action=filter]")
-    .addEventListener("click", clickFilterButton);
-}
-
+//Loading JSON data
 async function loadJSON() {
+  console.log("loadJSON");
   const response = await fetch("animals.json");
   const jsonData = await response.json();
 
-  // when loaded, prepare data objects
+  //When loaded, prepare data objects
   prepareObjects(jsonData);
 }
 
 function prepareObjects(jsonData) {
-  allAnimals = jsonData.map(preapareObject);
-
-  // TODO: This might not be the function we want to call first
-  //Creating a filterList function
-  function filterList(filteredList) {
-    // let filteredList = allAnimals;
-    if (settings.filterBy === "cat") {
-      // create a filtered list of only cats
-      filteredList = allAnimals.filter(isCat);
-    } else if (settings.filterBy === "dog") {
-      // create a filtered list of only dogs
-      filteredList = allAnimals.filter(isDog);
-    } else if (settings.filterBy === "star") {
-      filteredList = allAnimals.filter(isStar);
-    }
-
-    return filteredList;
-  }
-  //Filtering cats
-  function isCat(animal) {
-    return animal.type === "cat";
-  }
-
-  //Filtering dogs
-  function isDog(animal) {
-    return animal.type === "dog";
-  }
-
-  //Filtering star (all)
-  function isStar(animal) {
-    return animal.star;
-  }
-
-  //Filtering all
+  console.log("prepareObjects");
+  allAnimals = jsonData.map(prepareObject);
 
   displayList(allAnimals);
 }
 
-function preapareObject(jsonObject) {
+function prepareObject(jsonObject) {
+  console.log("prepareObject");
+
   const animal = Object.create(Animal);
 
   const texts = jsonObject.fullname.split(" ");
@@ -89,25 +62,85 @@ function preapareObject(jsonObject) {
 }
 
 function displayList(animals) {
-  // clear the list
+  console.log("displayList");
+
+  //Clear the list
   document.querySelector("#list tbody").innerHTML = "";
 
-  // build a new list
+  //Build a new list
   animals.forEach(displayAnimal);
 }
 
 function displayAnimal(animal) {
-  // create clone
+  console.log("displayAnimal");
+
+  //Create clone
   const clone = document
     .querySelector("template#animal")
     .content.cloneNode(true);
 
-  // set clone data
+  //set clone data
   clone.querySelector("[data-field=name]").textContent = animal.name;
   clone.querySelector("[data-field=desc]").textContent = animal.desc;
   clone.querySelector("[data-field=type]").textContent = animal.type;
   clone.querySelector("[data-field=age]").textContent = animal.age;
 
-  // append clone to list
+  //Append clone to list
   document.querySelector("#list tbody").appendChild(clone);
+}
+
+function clickFilterButton(event) {
+  filter = event.target.dataset.filter;
+  console.log(filter);
+
+  const filteredAnimals = filterAnimals();
+  console.log(filteredAnimals);
+  displayList(filteredAnimals);
+}
+
+//Filtering the animals
+function filterAnimals() {
+  console.log("filterAnimals");
+
+  let filteredAnimals = [];
+
+  switch (filter) {
+    case "all":
+      filteredAnimals = allAnimals.filter(isAll);
+      break;
+    case "cat":
+      filteredAnimals = allAnimals.filter(isCat);
+      break;
+    case "dog":
+      filteredAnimals = allAnimals.filter(isDog);
+      break;
+    default:
+      console.log("I don't know this animal :(");
+  }
+
+  console.log(filteredAnimals);
+  return filteredAnimals;
+}
+
+function isCat(animal) {
+  console.log("isCat");
+  if (animal.type === "cat") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isDog(animal) {
+  console.log("isDog");
+  if (animal.type === "dog") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isAll(animal) {
+  console.log("isAll");
+  return true;
 }
